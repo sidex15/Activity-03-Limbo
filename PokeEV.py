76833fb0f0a4cd08e845c77393f443a6e7abea6a
sidex15 <moses.limbo@gmail.com>
2022-04-01 16:14:53 +0800

    Change 'pokeModif' to 'nature' in 'ev' Variable

--- PokeEV.py
+++ PokeEV.py
@@ -1,4 +1,4 @@
-class pokeev():
+class PokeEV():
 
     def atkev(pokeStat,pokeModif,pokeBase,pokeLVL,pokeIV,pokeEV):
         nature = 1.0
@@ -7,9 +7,9 @@ class pokeev():
         elif pokeModif == 5 or pokeModif == 10 or pokeModif == 15 or pokeModif == 20:
             nature=0.9
         else: 
-            nature=1.0
-        d = ((2 * pokeBase + pokeIV +(pokeEV/4)*pokeLVL/100))
-        ev = (((((pokeStat/pokeModif)-d)*400)/pokeLVL)/4)*4
+            nature=0.9
+        d = ((2 * pokeBase + pokeIV +(pokeEV/4))*pokeLVL/100)
+        ev = (((((pokeStat/nature)-d)*400)/pokeLVL)/4)*4
         return ev
     
     def defev(pokeStat,pokeModif,pokeBase,pokeLVL,pokeIV,pokeEV):
@@ -19,9 +19,9 @@ class pokeev():
         elif pokeModif == 1 or pokeModif == 11 or pokeModif == 16 or pokeModif == 21:
             nature=0.9
         else: 
-            nature=1.0
-        d = ((2 * pokeBase + pokeIV +(pokeEV/4)*pokeLVL/100))
-        ev = (((((pokeStat/pokeModif)-d)*400)/pokeLVL)/4)*4
+            nature=0.9
+        d = ((2 * pokeBase + pokeIV +(pokeEV/4))*pokeLVL/100)
+        ev = (((((pokeStat/nature)-d)*400)/pokeLVL)/4)*4
         return ev
 
     def spatkev(pokeStat,pokeModif,pokeBase,pokeLVL,pokeIV,pokeEV):
@@ -31,9 +31,9 @@ class pokeev():
         elif pokeModif == 3 or pokeModif == 8 or pokeModif == 13 or pokeModif == 23:
             nature=0.9
         else: 
-            nature=1.0
-        d = ((2 * pokeBase + pokeIV +(pokeEV/4)*pokeLVL/100))
-        ev = (((((pokeStat/pokeModif)-d)*400)/pokeLVL)/4)*4
+            nature=0.9
+        d = ((2 * pokeBase + pokeIV +(pokeEV/4))*pokeLVL/100)
+        ev = (((((pokeStat/nature)-d)*400)/pokeLVL)/4)*4
         return ev
 
     def spdefev(pokeStat,pokeModif,pokeBase,pokeLVL,pokeIV,pokeEV):
@@ -43,9 +43,9 @@ class pokeev():
         elif pokeModif == 4 or pokeModif == 9 or pokeModif == 14 or pokeModif == 19:
             nature=0.9
         else: 
-            nature=1.0
-        d = ((2 * pokeBase + pokeIV +(pokeEV/4)*pokeLVL/100))
-        ev = (((((pokeStat/pokeModif)-d)*400)/pokeLVL)/4)*4
+            nature=0.9
+        d = ((2 * pokeBase + pokeIV +(pokeEV/4))*pokeLVL/100)
+        ev = (((((pokeStat/nature)-d)*400)/pokeLVL)/4)*4
         return ev
 
     def spdev(pokeStat,pokeModif,pokeBase,pokeLVL,pokeIV,pokeEV):
@@ -55,7 +55,7 @@ class pokeev():
         elif pokeModif == 2 or pokeModif == 7 or pokeModif == 17 or pokeModif == 22:
             nature=0.9
         else: 
-            nature=1.0
-        d = ((2 * pokeBase + pokeIV +(pokeEV/4)*pokeLVL/100))
-        ev = (((((pokeStat/pokeModif)-d)*400)/pokeLVL)/4)*4
+            nature=0.9
+        d = ((2 * pokeBase + pokeIV + (pokeEV/4))*pokeLVL/100)
+        ev = (((((pokeStat/nature)-d)*400)/pokeLVL)/4)*4
         return ev
