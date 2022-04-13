596971e94674ac87c31c4c5a0c6a0d8b9d74caa7
sidex15 <moses.limbo@gmail.com>
2022-04-13 13:27:54 +0800

    Change Each Stat as List Variable
    Have to separate the values of each stat in Stats Calculator and EV Calculator
    so make a stats a list variable (Stat= [Base,IV,EV]/Stat= [Base,value,IV,EV]).
    
    Also Added comments to make it understandable...

--- main.py
+++ main.py
@@ -1,46 +1,84 @@
 import PokeStat as stats
 import PokeEV as EV
-import math
-import sys
 import os
 
 def Calstat():
-    #print ("HP stat")
-    hpbase = int(input("Base HP/Stat: "))
-    hpIV = int(input("Individual Value (0-31): "))
-    if hpIV > 31:
+    #Stat= [Base,IV,EV]
+    hpstat = [0,0,0]
+    atkstat = [0,0,0]
+    defstat = [0,0,0]
+    spatkstat = [0,0,0]
+    spdefstat = [0,0,0]
+    spdstat = [0,0,0]
+
+    #Base Stats
+    hpstat[0] = int(input("Base HP: "))
+    atkstat[0] = int(input("Base Atk: "))
+    defstat[0] = int(input("Base Def: "))
+    spatkstat[0] = int(input("Base Sp.Atk: "))
+    spdefstat[0] = int(input("Base Sp.Def: "))
+    spdstat[0] = int(input("Base spd: "))
+
+    #IV Stats
+    hpstat[1] = int(input("\nHP Individual Value (0-31): "))
+    if hpstat[1] > 31:
         print("Just Select 0-31...\nExitting...")   
         exit()
-    hpEV = int(input("HP Effort Value (0-255): "))
-    if hpEV > 255:
+    atkstat[1] = int(input("Atk Individual Value (0-31): "))
+    if atkstat[1] > 31:
+        print("Just Select 0-31...\nExitting...")   
+        exit()
+    defstat[1] = int(input("Def Individual Value (0-31): "))
+    if defstat[1] > 31:
+        print("Just Select 0-31...\nExitting...")   
+        exit()
+    spatkstat[1] = int(input("Sp.Atk Individual Value (0-31): "))
+    if spatkstat[1] > 31:
+        print("Just Select 0-31...\nExitting...")   
+        exit()
+    spdefstat[1] = int(input("Sp.def Individual Value (0-31): "))
+    if spdefstat[1] > 31:
+        print("Just Select 0-31...\nExitting...")   
+        exit()
+    spdstat[1] = int(input("Spd Individual Value (0-31): "))
+    if spdstat[1] > 31:
+        print("Just Select 0-31...\nExitting...")   
+        exit()
+
+    #EV Stats
+    hpstat[2] = int(input("\nHP Effort Value (0-255): "))
+    if hpstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    atkEV = int(input("Atk Effort Value (0-255): "))
-    if atkEV > 255:
+    atkstat[2] = int(input("Atk Effort Value (0-255): "))
+    if atkstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    defEV = int(input("Def Effort Value (0-255): "))
-    if defEV > 255:
+    defstat[2] = int(input("Def Effort Value (0-255): "))
+    if defstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    spatkEV = int(input("Sp.Atk Effort Value (0-255): "))
-    if spatkEV > 255:
+    spatkstat[2] = int(input("Sp.Atk Effort Value (0-255): "))
+    if spatkstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    spdefEV = int(input("Sp.Def Effort Value (0-255): "))
-    if spdefEV > 255:
+    spdefstat[2] = int(input("Sp.Def Effort Value (0-255): "))
+    if spdefstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    spdEV = int(input("Spd Effort Value (0-255): "))
-    if spdEV > 255:
+    spdstat[2] = int(input("Spd Effort Value (0-255): "))
+    if spdstat[2] > 255:
         print("Just Select 0-255...\nExitting...")   
         exit()
-    evlim = atkEV+defEV+spatkEV+spdefEV+spdEV
+    evlim = atkstat[2]+defstat[2]+spatkstat[2]+spdefstat[2]+spdstat[2]
     if evlim > 510:
         print("total EV limit reached...\nshould not exeed 510...\nexitting...")
         exit()
+
+    #Pokemon Level
     hplvl = int(input("Pokemon Level: "))
 
+    #Nature
     pntr = """
     0 - Hardy       5 - Bold        10 - Timid      15 - Modest     20 - Calm
     1 - Lonely      6 - Docile      11 - Hasty      16 - Mild       21 - Gentle
@@ -54,13 +92,15 @@ def Calstat():
         print("Just Select 0-24...\nExitting...")   
         exit()
 
-    stathp = stats.PokeStat.hpstat(hpbase, hpIV, hpEV, hplvl)
-    statatk = stats.PokeStat.atkstat(hpbase, hpIV, atkEV, hplvl, oNtr)
-    statdef = stats.PokeStat.defstat(hpbase, hpIV, defEV, hplvl, oNtr)
-    statspatk = stats.PokeStat.spatkstat(hpbase, hpIV, spatkEV, hplvl, oNtr)
-    statspdef = stats.PokeStat.spdefstat(hpbase, hpIV, spdefEV, hplvl, oNtr)
-    statspd = stats.PokeStat.spdstat(hpbase, hpIV, spdEV, hplvl, oNtr)
+    #Stats Computation
+    stathp = stats.PokeStat.hpstat(hpstat[0], hpstat[1], hpstat[2], hplvl)
+    statatk = stats.PokeStat.atkstat(atkstat[0], atkstat[1], atkstat[2], hplvl, oNtr)
+    statdef = stats.PokeStat.defstat(defstat[0], defstat[1], defstat[2], hplvl, oNtr)
+    statspatk = stats.PokeStat.spatkstat(spatkstat[0], spatkstat[1], spatkstat[2], hplvl, oNtr)
+    statspdef = stats.PokeStat.spdefstat(spdefstat[0], spdefstat[1], spdefstat[2], hplvl, oNtr)
+    statspd = stats.PokeStat.spdstat(spdstat[0], spdstat[1], spdstat[2], hplvl, oNtr)
 
+    #Stats Result
     print("HP: ", stathp)
     print("Atk: ", statatk)
     print("Def: ", statdef)
@@ -69,6 +109,13 @@ def Calstat():
     print("Spd: ", statspd)
 
 def EVstat():
+    #Stat= [Base,value,IV,EV]
+    atkstat = [0,0,0,0]
+    defstat = [0,0,0,0]
+    spatkstat = [0,0,0,0]
+    spdefstat = [0,0,0,0]
+    spdstat = [0,0,0,0]
+
     print("1 - Single Stat\n2 - All Stats")
     evinp = int(input("select (1-2): "))
     if evinp >=3:
@@ -88,43 +135,85 @@ def EVstat():
             print("Just Select 1-5...\nExitting...")   
             exit()
     
-    stbase = int(input("Base Stat: "))
-    stIV = int(input("Individual Value (0-31): "))
-    if stIV > 31:
-        print("Just Select 0-31...\nExitting...")   
-        exit()
+    
     if evinp == 1:
+        stbase = int(input("Base Stat: "))
+        stinc = int(input("Desired increase Stat: "))
+        stIV = int(input("Individual Value (0-31): "))
+        if stIV > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
         stEV = int(input("Effort Value (0-255): "))
         if stEV > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
     else:
-        atkEV = int(input("Atk Effort Value (0-255): "))
-        if atkEV > 255:
+        #Base Stats
+        atkstat[0] = int(input("Base Atk: "))
+        defstat[0] = int(input("Base Def: "))
+        spatkstat[0] = int(input("Base Sp.Atk: "))
+        spdefstat[0] = int(input("Base Sp.Def: "))
+        spdstat[0] = int(input("Base spd: "))
+
+        #Desired increase Stats
+        atkstat[1] = int(input("\nDesired increase Atk: "))
+        defstat[1] = int(input("Desired increasee Def: "))
+        spatkstat[1] = int(input("Desired increase Sp.Atk: "))
+        spdefstat[1] = int(input("Desired increase Sp.Def: "))
+        spdstat[1] = int(input("Desired increase spd: "))
+
+        #IV Stats
+        atkstat[2] = int(input("\nAtk Individual Value (0-31): "))
+        if atkstat[2] > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
+        defstat[2] = int(input("Def Individual Value (0-31): "))
+        if defstat[2] > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
+        spatkstat[2] = int(input("Sp.Atk Individual Value (0-31): "))
+        if spatkstat[2] > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
+        spdefstat[2] = int(input("Sp.def Individual Value (0-31): "))
+        if spdefstat[2] > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
+        spdstat[2] = int(input("Spd Individual Value (0-31): "))
+        if spdstat[2] > 31:
+            print("Just Select 0-31...\nExitting...")   
+            exit()
+
+        #EV Stats
+        atkstat[3] = int(input("\nAtk Effort Value (0-255): "))
+        if atkstat[3] > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
-        defEV = int(input("Def Effort Value (0-255): "))
-        if defEV > 255:
+        defstat[3] = int(input("Def Effort Value (0-255): "))
+        if defstat[3] > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
-        spatkEV = int(input("Sp.Atk Effort Value (0-255): "))
-        if spatkEV > 255:
+        spatkstat[3] = int(input("Sp.Atk Effort Value (0-255): "))
+        if spatkstat[3] > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
-        spdefEV = int(input("Sp.Def Effort Value (0-255): "))
-        if spdefEV > 255:
+        spdefstat[3] = int(input("Sp.Def Effort Value (0-255): "))
+        if spdefstat[3] > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
-        spdEV = int(input("Spd Effort Value (0-255): "))
-        if spdEV > 255:
+        spdstat[3] = int(input("Spd Effort Value (0-255): "))
+        if spdstat[3] > 255:
             print("Just Select 0-255...\nExitting...")   
             exit()
-        evlim = atkEV+defEV+spatkEV+spdefEV+spdEV
+        evlim = atkstat[3]+defstat[3]+spatkstat[3]+spdefstat[3]+spdstat[3]
         if evlim > 510:
             print("total EV limit reached...\nshould not exeed 510...\nexitting...")
             exit()
 
+    #Level
     stlvl = int(input("Pokemon Level: "))
+
+    #Nature
     pntr = """
     0 - Hardy       5 - Bold        10 - Timid      15 - Modest     20 - Calm
     1 - Lonely      6 - Docile      11 - Hasty      16 - Mild       21 - Gentle
@@ -135,19 +224,22 @@ def EVstat():
     print (pntr)
     stNtr = int(input("Modifier (Nature): "))
     
+    #This Part should Calculate the desired increase stats first
+    #using the Stats Calculator to prevent negative result in EVs
     if evinp == 1:
-        stStatatk = stats.PokeStat.atkstat(stbase, stIV, stEV, stlvl, stNtr)
-        stStatdef = stats.PokeStat.defstat(stbase, stIV, stEV, stlvl, stNtr)
-        stStatspatk = stats.PokeStat.spatkstat(stbase, stIV, stEV, stlvl, stNtr)
-        stStatspdef = stats.PokeStat.spdefstat(stbase, stIV, stEV, stlvl, stNtr)
-        stStatspd = stats.PokeStat.spdstat(stbase, stIV, stEV, stlvl, stNtr)
+        stStatatk = stats.PokeStat.atkstat(stinc, stIV, stEV, stlvl, stNtr)
+        stStatdef = stats.PokeStat.defstat(stinc, stIV, stEV, stlvl, stNtr)
+        stStatspatk = stats.PokeStat.spatkstat(stinc, stIV, stEV, stlvl, stNtr)
+        stStatspdef = stats.PokeStat.spdefstat(stinc, stIV, stEV, stlvl, stNtr)
+        stStatspd = stats.PokeStat.spdstat(stinc, stIV, stEV, stlvl, stNtr)
     else:
-        stStatatk = stats.PokeStat.atkstat(stbase, stIV, atkEV, stlvl, stNtr)
-        stStatdef = stats.PokeStat.defstat(stbase, stIV, defEV, stlvl, stNtr)
-        stStatspatk = stats.PokeStat.spatkstat(stbase, stIV, spatkEV, stlvl, stNtr)
-        stStatspdef = stats.PokeStat.spdefstat(stbase, stIV, spdefEV, stlvl, stNtr)
-        stStatspd = stats.PokeStat.spdstat(stbase, stIV, spdEV, stlvl, stNtr)
+        stStatatk = stats.PokeStat.atkstat(atkstat[1], atkstat[2], atkstat[3], stlvl, stNtr)
+        stStatdef = stats.PokeStat.defstat(defstat[1], defstat[2], defstat[3], stlvl, stNtr)
+        stStatspatk = stats.PokeStat.spatkstat(spatkstat[1], spatkstat[2], spatkstat[3], stlvl, stNtr)
+        stStatspdef = stats.PokeStat.spdefstat(spdefstat[1], spdefstat[2], spdefstat[3], stlvl, stNtr)
+        stStatspd = stats.PokeStat.spdstat(spdstat[1], spdstat[2], spdstat[3], stlvl, stNtr)
 
+    #After Calculating the desired increase stats now for the EV calculation
     if evinp == 1:
         evatk = EV.PokeEV.atkev(stStatatk, stNtr, stbase, stlvl, stIV, stEV)
         evdef = EV.PokeEV.defev(stStatdef, stNtr, stbase, stlvl, stIV, stEV)
@@ -155,12 +247,13 @@ def EVstat():
         evspdef = EV.PokeEV.spdefev(stStatspdef, stNtr, stbase, stlvl, stIV, stEV)
         evspd = EV.PokeEV.spdev(stStatspd, stNtr, stbase, stlvl, stIV, stEV)
     else:
-        evatk = EV.PokeEV.atkev(stStatatk, stNtr, stbase, stlvl, stIV, atkEV)
-        evdef = EV.PokeEV.defev(stStatdef, stNtr, stbase, stlvl, stIV, defEV)
-        evspatk = EV.PokeEV.spatkev(stStatspatk, stNtr, stbase, stlvl, stIV, spatkEV)
-        evspdef = EV.PokeEV.spdefev(stStatspdef, stNtr, stbase, stlvl, stIV, spdefEV)
-        evspd = EV.PokeEV.spdev(stStatspd, stNtr, stbase, stlvl, stIV, spdEV)
+        evatk = EV.PokeEV.atkev(stStatatk, stNtr, atkstat[0], stlvl, atkstat[2], atkstat[3])
+        evdef = EV.PokeEV.defev(stStatdef, stNtr, defstat[0], stlvl, defstat[2], defstat[3])
+        evspatk = EV.PokeEV.spatkev(stStatspatk, stNtr, spatkstat[0], stlvl, spatkstat[2], spatkstat[3])
+        evspdef = EV.PokeEV.spdefev(stStatspdef, stNtr, spdefstat[0], stlvl, spdefstat[2], spdefstat[3])
+        evspd = EV.PokeEV.spdev(stStatspd, stNtr, spdstat[0], stlvl, spdstat[2], spdstat[3])
 
+    #EV Result
     if evinp == 2:
         print("\nAtk: ", evatk)
         print("Def: ", evdef)
